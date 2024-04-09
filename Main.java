a04226eb66f334f98e0a8bd0bff8aa36e6407d05
LeoGynn <92831967+LeoGynn@users.noreply.github.com>
2024-04-09 18:23:43 -0300

    Create Main.java
    Exercicio 4

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Instanciando dois objetos da classe Produto
+        Produto cocaCola = new Produto();
+        Produto cocaColaZero = new Produto();
+
+        // Alterando os atributos dos objetos
+        cocaCola.nome = "Coca-Cola";
+        cocaCola.preco = 1.5;
+        cocaCola.quantidadeEmEstoque = 100;
+
+        cocaColaZero.nome = "Coca-Cola Zero";
+        cocaColaZero.preco = 1.5;
+        cocaColaZero.quantidadeEmEstoque = 100;
+
+        // Invocando os métodos
+        cocaCola.adicionarAoCarrinho();
+        cocaColaZero.removerDoCarrinho();
+    }
+}
